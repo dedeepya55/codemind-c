1bd1021e50ee7c2ac6ad94cba597ba37ba37569f
dedeepya55 <119920060+dedeepya55@users.noreply.github.com>
2022-12-20 20:49:24 +0530

    #include<stdio.h> int main() {     int i,n,c=0,t=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)         {             if(a[i]%2!=0)             c+=1;         }     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         t+=1;     }     if(c==t)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,c=0,t=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0)
+        {
+            if(a[i]%2!=0)
+            c+=1;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        t+=1;
+    }
+    if(c==t)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
